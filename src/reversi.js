c1d47a928f4fdc396a033c0ee16e0b1d71408753
ikjae1002 <ij374@nyu.edu>
2017-09-16 17:32:42 -0400

    finished repeat function

--- src/reversi.js
+++ src/reversi.js
@@ -2,6 +2,14 @@
 
 function repeat(ele, n){
     //implementation
+    let i;
+    const arr = new Array;
+    for(i = 0; i < n; i++){
+        //console.log(arr);
+        arr.push(ele);
+    }
+    
+    return arr;
 }
 
 function generateBoard(rows, cols, initialValue){
@@ -15,3 +23,6 @@ module.exports = {
     repeat: repeat,
     generateBoard: generateBoard    
 }
+
+const arr = repeat(4,5);
+console.log(arr);
