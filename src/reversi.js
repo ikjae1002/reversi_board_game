129a3b3454eaf9df756db82c1ad8b7aa72d3a3fa
ikjae1002 <ij374@nyu.edu>
2017-09-16 17:50:15 -0400

    added generateBoard function

--- src/reversi.js
+++ src/reversi.js
@@ -1,7 +1,8 @@
 // reversi.js
 
 function repeat(ele, n){
-    //implementation
+    // repeat creates an array that contains value as each element
+    // for n elements.
     let i;
     const arr = new Array;
     for(i = 0; i < n; i++){
@@ -12,10 +13,19 @@ function repeat(ele, n){
     return arr;
 }
 
-function generateBoard(rows, cols, initialValue){
-    //implementation
+function generateBoard(rows, cols, initialValue = " "){
+    // creates a single dimensional array representation
+    // of elements that would be in a rows x columns board
+    // with each cell containing the initial value
+    const mul = rows * cols;
+    const board = repeat(initialValue, mul);
+    
+    return board;
 }
 
+function rowColToIndex(board, rowNumber, columnNumber){
+
+}
 
 //module exports
 
@@ -24,5 +34,5 @@ module.exports = {
     generateBoard: generateBoard    
 }
 
-const arr = repeat(4,5);
-console.log(arr);
+const board = generateBoard(3,3);
+console.log(board);
