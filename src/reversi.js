f8720ef3a25dd7ea38085f20f012d8ef5ff97f44
ikjae1002 <ij374@nyu.edu>
2017-09-16 17:13:33 -0400

    updating functions

--- src/reversi.js
+++ src/reversi.js
@@ -1,1 +1,17 @@
 // reversi.js
+
+function repeat(ele, n){
+    //implementation
+}
+
+function generateBoard(rows, cols, initialValue){
+    //implementation
+}
+
+
+//module exports
+
+module.exports = {
+    repeat: repeat,
+    generateBoard: generateBoard    
+}
